2b5c4e3c29db19bb8b624c13c0bc5da5c45461bd
Atril33 <118503473+Atril33@users.noreply.github.com>
2023-04-18 15:14:03 +0500

    Fetch Rockets data from API

--- src/components/Redux/RocketsSlice/RocketSlice.js
+++ src/components/Redux/RocketsSlice/RocketSlice.js
@@ -1,13 +1,37 @@
-import { createSlice } from '@reduxjs/toolkit';
-import data from '../../TempRocketData';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+/* import data from '../../TempRocketData'; */
+
+const url = 'https://api.spacexdata.com/v3/rockets';
 
 const initialState = {
-  RocketDatile: data,
+  RocketDatile: [],
+  loading: false,
+  error: null,
 };
+
+export const rocketItems = createAsyncThunk('rocket', async () => {
+  const response = await fetch(url);
+  const result = response.json();
+  return result;
+});
 
 const rocketSlice = createSlice({
   name: 'rocket',
   initialState,
+  extraReducers: {
+    [rocketItems.pending]: (state) => {
+      state.loading = true;
+    },
+    [rocketItems.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.RocketDatile = action.payload;
+    },
+    [rocketItems.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+
+  },
 });
 
 export default rocketSlice.reducer;
--- src/components/Rockets.jsx
+++ src/components/Rockets.jsx
@@ -1,16 +1,23 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import './style.css';
+import { useEffect } from 'react';
+import { rocketItems } from './Redux/RocketsSlice/RocketSlice';
 
 const Rockets = () => {
   const { RocketDatile } = useSelector((store) => store.rocket);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(rocketItems());
+  }, [dispatch]);
+
   return (
     <div className="rocket-container">
       {RocketDatile.map((item) => (
-        <div className="rocket-items" key={item.name}>
-          <img src={item.image} alt="Rocket" />
+        <div className="rocket-items" key={item.rocket_id}>
+          <img src={item.flickr_images[0]} alt="Rocket" />
           <ul className="rocket-detail">
-            <li><h2 className="rocket-name">{item.name}</h2></li>
-            <li><p className="rocket-des">{item.des}</p></li>
+            <li><h2 className="rocket-name">{item.rocket_name}</h2></li>
+            <li><p className="rocket-des">{item.description}</p></li>
             <li><button type="button" className="reserve-bttn">reserve rocket</button></li>
           </ul>
         </div>
